e61262a3289638f1a28e2633f8e042bf93297b90
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2020-07-18 03:17:01 +0800

    pang basic game logic completed

---
+++ GDD Project/Assets/Scripts/Pang Scripts/Ball.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float forceX, forceY;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private bool moveLeft;
+    [SerializeField]
+    private bool moveRight;
+    [SerializeField]
+    private GameObject originalBall; // XL ball
+    private GameObject ball1, ball2; //split the ball into 2 smaller balls
+    private Ball ball1Script, ball2Script; // need scripts for the smaller balls to manipulate their speed and direction
+    [SerializeField]
+    private AudioClip[] popSounds; // array bcos got 2 balls
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SetBallSpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBall();
+    }
+
+    void InstantiateBalls()
+    {
+        if(this.gameObject.tag != "XS Ball")
+        {
+            // split the ball into 2 smaller balls
+            ball1 = Instantiate(originalBall);
+            ball2 = Instantiate(originalBall);
+
+            ball1.name = originalBall.name; // this is to prevent having Ball(Clone) instead of Ball when Split in Rocket.cs
+            ball2.name = originalBall.name;
+
+            ball1Script = ball1.GetComponent<Ball>();
+            ball2Script = ball2.GetComponent<Ball>();
+        }
+    }
+
+
+    void InitializeBallandDisableCurrentBall()
+    {
+        InstantiateBalls();
+
+        // get position of balls
+        Vector3 temp = transform.position;
+
+        ball1.transform.position = temp;
+        ball1Script.SetMoveLeft(true);
+
+        ball2.transform.position = temp;
+        ball2Script.SetMoveRight(true);
+
+        // give the ball some boost
+        ball1.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 2.5f);
+        ball2.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 2.5f);
+
+        AudioSource.PlayClipAtPoint(popSounds[Random.Range(0, popSounds.Length)], transform.position); // play popSounds at position
+        gameObject.SetActive(false);
+    }
+
+    public void SetMoveLeft(bool canMoveLeft)
+    {
+        this.moveLeft = canMoveLeft;
+        this.moveRight = !canMoveLeft; // if moveLeft is true, ! will make it false
+    }
+
+    public void SetMoveRight(bool canMoveRight)
+    {
+        this.moveRight = canMoveRight;
+        this.moveLeft = !canMoveRight;
+    }
+
+    void MoveBall()
+    {
+        // TODO: Randomize where the ball is being spawned
+        if (moveLeft)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= forceX * Time.deltaTime; // move Left = negative
+            transform.position = temp;
+        }
+
+        if (moveRight)
+        {
+            Vector3 temp = transform.position;
+            temp.x += forceX * Time.deltaTime; // move Right = npositive
+            transform.position = temp;
+        }
+    }
+
+    void SetBallSpeed()
+    {
+        forceX = 2.5f; // same force for all balls
+
+        // manipulate forceY for different sizes of balls to set ball speed
+        switch (this.gameObject.tag)
+        {
+            case "XL Ball":
+                forceY = 11.5f;
+                break;
+
+            case "L Ball":
+                forceY = 10.5f;
+                break;
+
+            case "M Ball":
+                forceY = 9f;
+                break;
+
+            case "S Ball":
+                forceY = 8f;
+                break;
+
+            case "XS Ball":
+                forceY = 7f;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // bounce ball
+        // if ball touched the ground
+        if (collision.tag == "Ground")
+        {
+            rb.velocity = new Vector2(0, forceY);
+        }
+
+        if (collision.tag == "Right Wall")
+        {
+            SetMoveLeft(true);
+            //Old ver
+            // if collide with Right Wall will start bouncing towards the left
+            //moveRight = false;
+            //moveLeft = true;
+        }
+
+        if (collision.tag == "Left Wall")
+        {
+            SetMoveRight(true);
+            // Old ver
+            // if collide with Left Wall will start bouncing towards the right
+            //moveRight = true;
+            //moveLeft = false;
+        }
+
+        if (collision.tag == "Rocket")
+        {
+            if(gameObject.tag != "XS Ball")
+            {
+                InitializeBallandDisableCurrentBall();
+            }
+            else
+            {
+                AudioSource.PlayClipAtPoint(popSounds[Random.Range(0, popSounds.Length)], transform.position);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ GDD Project/Assets/Scripts/Pang Scripts/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, speed); //move rocket further up the y-axis
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        // check if rocket collides with Top
+        if (target.tag == "Top")
+        {
+            Destroy(gameObject);
+        }
+
+        string[] name = target.name.Split(); // get name of gameobject that the ball collides with
+        // Why use Split?
+        // e.g. string s = "XL Ball"; 
+        // Split will seperate XL Ball into 'XL' and 'Ball', which can be utilised for different purposes
+        for (int i = 0; i < name.Length; i++)
+        {
+            Debug.Log("The array contains " + name[i]);
+        }
+        
+        if( name.Length > 1)
+        {
+            if (name[1] == "Ball")
+            {
+                Destroy(gameObject); // when rocket hit ball, rocket gets destroyed
+            }
+
+
+        }
+        
+        
+    }
+}
