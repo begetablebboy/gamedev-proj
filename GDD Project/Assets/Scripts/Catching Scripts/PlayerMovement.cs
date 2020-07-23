4a88a2568505e8ef0934d61e5e4ec4731bb52e3b
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2020-07-23 18:04:46 +0800

    Catching Blobby anim done

--- GDD Project/Assets/Scripts/Catching Scripts/PlayerMovement.cs
+++ GDD Project/Assets/Scripts/Catching Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -7,16 +8,50 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Rigidbody2D myBody;
 
+    private Animator anim;
+    private bool canWalk;
+
 	void Awake () {
 		myBody = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+        canWalk = true;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector2 vel = myBody.velocity;
-		vel.x = Input.GetAxis ("Horizontal") * speed;
+        float h = Input.GetAxis("Horizontal");
+        vel.x = h * speed;
 		myBody.velocity = vel;
-	}
+
+        if (canWalk)
+        {
+            // moving right
+            if (h > 0)
+            {
+
+                Vector3 scale = transform.localScale; // get player current scale
+                scale.x = 0.6f;
+                transform.localScale = scale; // face the Right direction
+                anim.SetBool("Walk", true);
+            }
+            //moving left
+            else if (h < 0)
+            {
+
+                Vector3 scale = transform.localScale;
+                scale.x = -0.6f;
+                transform.localScale = scale; // face the Left direction
+                anim.SetBool("Walk", true);
+            }
+            else if (h == 0)
+            {
+                anim.SetBool("Walk", false); //idle
+            }
+
+        }
+
+    }
 
 
 } // class
