b5902ef09b6f7a9c92bf8defb05534faa699e468
Kenneth <kenneth_soon@mymail.sutd.edu.sg>
2020-07-24 16:05:07 +0800

    Added Tron2D game
    Basic Tron2D lightcycle game added

---
+++ Tron2D/Assets/Scripts/Move.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Movement keys (customizable in Inspector)
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+
+    // Movement Speed
+    public float speed = 16;
+
+    // Wall Prefab
+    public GameObject wallPrefab;
+
+    // Current Wall
+    Collider2D wall;
+
+    // Last Wall's End
+    Vector2 lastWallEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        spawnWall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for key presses
+        if (Input.GetKeyDown(upKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(downKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(rightKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(leftKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
+            spawnWall();
+        }
+
+        fitColliderBetween(wall, lastWallEnd, transform.position);
+    }
+
+    void spawnWall()
+    {
+        // Save last wall's position
+        lastWallEnd = transform.position;
+        // Spawn a new Lightwall
+        GameObject g = Instantiate(wallPrefab, transform.position, Quaternion.identity);
+        wall = g.GetComponent<Collider2D>();
+    }
+
+    void fitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
+    {
+        // Calculate the Center Position
+        co.transform.position = a + (b - a) * 0.5f;
+
+        // Scale it (horizontally or vertically)
+        float dist = Vector2.Distance(a, b);
+        if (a.x != b.x)
+            co.transform.localScale = new Vector2(dist + 1, 1);
+        else
+            co.transform.localScale = new Vector2(1, dist + 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        // Not the current wall?
+        if (co != wall)
+        {
+            print("Player lost: " + name);
+            Destroy(gameObject);
+        }
+    }
+}
