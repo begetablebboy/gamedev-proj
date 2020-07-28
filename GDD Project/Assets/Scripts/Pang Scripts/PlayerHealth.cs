15d815a15588438b9d20c17433902df154979cb3
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2020-07-28 21:13:13 +0800

    Chicky anim in Catching done

--- GDD Project/Assets/Scripts/Pang Scripts/PlayerHealth.cs
+++ GDD Project/Assets/Scripts/Pang Scripts/PlayerHealth.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int health;
+    public int numofhearts;
+    public Image[] hearts;
+    public Sprite
+
     // Start is called before the first frame update
     void Start()
     {
