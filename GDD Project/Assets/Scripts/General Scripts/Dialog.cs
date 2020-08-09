91c5561555478a2991e192455d299d6fdd95b66f
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2020-08-10 01:17:50 +0800

    pregame scene done

---
+++ GDD Project/Assets/Scripts/General Scripts/Dialog.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingspeed;
+    public GameObject continueButton;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true); // prevent spam click continue
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingspeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            //textDisplay.text = ""; // reset text when complete
+            SceneManager.LoadScene("Scenes/Pang Intro");
+        }
+    }
+}
