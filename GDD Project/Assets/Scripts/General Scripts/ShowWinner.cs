15adec25dcaf5ef87e96703c90e1081b6da3a14b
Sumedha <sumedha_gn@mymail.sutd.edu.sg>
2020-08-10 02:17:02 +0800

    winning scene done

---
+++ GDD Project/Assets/Scripts/General Scripts/ShowWinner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShowWinner : MonoBehaviour
+{
+    public TextMeshProUGUI winText;
+    public GameObject blobbyWin;
+    public GameObject chickyWin;
+    public GameObject bothWin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whoWin();
+        //Debug.Log(PlayerPrefs.GetInt("Player1"));
+        //Debug.Log(PlayerPrefs.GetInt("Player2"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void whoWin()
+    {
+        if(PlayerPrefs.GetInt("Player1") > PlayerPrefs.GetInt("Player2"))
+        {
+            winText.text = "P1 Wins!";
+            blobbyWin.SetActive(true);
+            chickyWin.SetActive(false);
+            bothWin.SetActive(false);
+        }
+        else if (PlayerPrefs.GetInt("Player1") < PlayerPrefs.GetInt("Player2"))
+        {
+            winText.text = "P2 Wins!";
+            chickyWin.SetActive(true);
+            blobbyWin.SetActive(false);
+            bothWin.SetActive(false);
+        }
+        else
+        {
+            winText.text = "Everyone is a winner!"; //draw situation
+            bothWin.SetActive(true);
+            blobbyWin.SetActive(false);
+            chickyWin.SetActive(false);
+        }
+    }
+}
